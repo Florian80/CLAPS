c20035590f2887209394927376d31e6a66c2bac4
Florian <florianheld80@gmail.com>
2017-12-10 21:58:17 +0100

    Some add to last commit.

---
+++ patientpath/src/main/java/claps/patientpath/BrowserCheckTest.java
@@ -0,0 +1,46 @@
+package claps.patientpath;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.server.Page;
+import com.vaadin.server.Page.BrowserWindowResizeEvent;
+import com.vaadin.server.WebBrowser;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.VerticalLayout;
+
+
+@SuppressWarnings("serial")
+public class BrowserCheckTest extends VerticalLayout implements View {
+	
+	/**
+	Page page = BrowserCheckTest.getPage();
+	page.addBrowserWindowResizeListener(new BrowserWindowResizeListener() {
+	  public void browserWindowResized(BrowserWindowResizeEvent event) {
+	    Notification.show("Window width=" + event.getWidth() + ", height=" + event.getHeight());
+	  }
+	});
+	
+	public WebBrowser browser = new WebBrowser();
+	
+	private Label labelHeader = new Label("IP: " + browser.getAddress() +
+            "User-Agent: " + browser.getBrowserApplication() +
+            "Linux: " + browser.isLinux());
+	**/
+	
+	public BrowserCheckTest() {
+	    
+		setSizeFull();
+		setSpacing(true);
+		//addComponent(labelHeader);
+	    
+	  }
+
+	@Override
+	public void enter(ViewChangeEvent event) {
+		Notification.show("IP" + Page.getCurrent().getWebBrowser());
+	}
+	
+}
+
+
---
+++ patientpath/src/main/java/claps/patientpath/ProviderTest.java
@@ -0,0 +1,21 @@
+package claps.patientpath;
+
+import com.vaadin.navigator.View;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
+
+
+@SuppressWarnings("serial")
+public class ProviderTest extends VerticalLayout implements View {
+
+	private Label labelHeader = new Label("Provider");
+	
+	public ProviderTest() {
+	    
+		setSizeFull();
+		setSpacing(true);
+		addComponent(labelHeader);
+	    
+	  }
+
+}
