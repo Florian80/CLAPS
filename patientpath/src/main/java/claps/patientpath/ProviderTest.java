861b8930bdb607b3bac50c0e3c9d60884e4c2ecd
Florian <florianheld80@gmail.com>
2017-12-16 22:16:21 +0100

    Solved "Select" issue in Grid

--- patientpath/src/main/java/claps/patientpath/ProviderTest.java
+++ patientpath/src/main/java/claps/patientpath/ProviderTest.java
@@ -1,15 +1,24 @@
 package claps.patientpath;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
 import com.vaadin.navigator.View;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
+import com.vaadin.ui.components.grid.ItemClickListener;
+import com.vaadin.ui.components.grid.SingleSelectionModel;
 import com.vaadin.ui.renderers.ButtonRenderer;
 import com.vaadin.ui.Grid.SelectionMode;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Notification;
+import com.vaadin.ui.SingleSelect;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Window;
 
@@ -37,16 +46,65 @@ public class ProviderTest extends VerticalLayout implements View {
 			
 			ProviderDAO providerDAO = new ProviderDAO();
 			
-			Grid<Provider> myGrid = new Grid();
-			myGrid.setSelectionMode(SelectionMode.NONE);
+			Grid<Provider> myGrid = new Grid<Provider>();
+			myGrid.setSelectionMode(SelectionMode.SINGLE);
+			SingleSelect<Provider> selection = myGrid.asSingleSelect();
+			//SingleSelectionModel<Provider> defaultModel = (SingleSelectionModel<Provider>) myGrid.getSelectionModel();
+			//SingleSelectionModel<Provider> singleSelect = (SingleSelectionModel<Provider>) myGrid.getSelectionModel();
 			myGrid.addColumn(claps.persistence.Provider::getProviderName).setCaption("Name");
+			myGrid.setItems(providerDAO.findAllProvider());
+			myGrid.addSelectionListener(event -> {
+				Notification.show(selection.getValue().getProviderinfoID() + " was selected");
+
+				/**
+				Provider provider = new Provider();
+			    Set <Provider> selected = event.getAllSelectedItems();
+			    Iterator iterator = new selected.Iterator();
+			    **/
+	
+			});
+			
+			
 			myGrid.addColumn(Provider -> "Info",
 				      new ButtonRenderer(clickEvent -> {
 				          System.out.println("yep");
 				          MyUI.getCurrent().addWindow(subWindowModalExample());
 				    }));
-			myGrid.setItems(providerDAO.findAllProvider());
+			
+			
+			/**
+			myGrid.addItemClickListener(new ItemClickListener() {
+				
+				public void itemClick(ItemClickEvent event) {
+					
+					if(event.isDoubleClick()) {
+						
+						Object itemID = event.getItemID();
+						
+						
+					}
+				}
+				
+			});
+			**/
+			
 
+			/**
+			myGrid.addSelectionListener(event -> {
+				System.out.println("Hallo: " + event.getValue().toString());
+			    Notification.show("Fuck you");
+			});
+			**/
+			
+			/**
+			myGrid.addSelectionListener(selectionEvent -> {
+				
+				System.out.println("Go fuck yourself");
+	            	
+			});
+			**/
+			
+			
 			return myGrid;
 		}
 
